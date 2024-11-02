71008cc7396221b37f25f2fa1e0591788094a96d
mkouchi <kouchi.mohammad@gmail.com>
2024-11-02 07:51:40 +0330

    Add Ctrl+c signal handler

--- crawler/main.py
+++ crawler/main.py
@@ -1,4 +1,16 @@
-from crawler.utils import setup_logging, create_directories, get_timestamp
+from crawler.utils import setup_logging, create_directories, get_timestamp, save_json
+import logging
+import signal
+
+# Signal handler for graceful exit on Ctrl+C
+def signal_handler(sig, frame):
+    print("\n\nCtrl+C detected. Stopping the crawl process...")
+    logging.info(f"Total documents scraped: {len(articles_content)}")
+    save_json(articles_content, start_url=start_url)
+    exit(0)
+
+# Register the signal handler for Ctrl+C (SIGINT)
+signal.signal(signal.SIGINT, signal_handler)
 
 def main():
     # setup logging
