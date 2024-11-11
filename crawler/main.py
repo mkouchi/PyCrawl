d1765e7503963557fb1ec2a30506de554f49bb29
mkouchi <kouchi.mohammad@gmail.com>
2024-11-11 16:48:55 +0330

    Implement and Fix main function

--- crawler/main.py
+++ crawler/main.py
@@ -1,23 +1,28 @@
 from urllib.parse import urlparse
-from crawler.robots_sitemaps_parser import fetch_and_parse_robots_txt, fetch_and_parse_sitemaps
-from crawler.scraper import crawl_website, scrape_url
-from crawler.utils import setup_logging, create_directories, get_timestamp, save_json
+from config import DEFAULT_USER_AGENT, MAX_CRAWL_COUNT
+from robots_sitemaps_parser import fetch_and_parse_robots_txt, fetch_and_parse_sitemaps
+from scraper import crawl_website, scrape_url
+from utils import setup_logging, create_directories, get_timestamp, save_json
 import logging
 import signal
 
+visited = set()  # Keep track of visited URLs
+articles = []  # Store crawled articles' content
 
-start_url = "https://www.hra-news.org/"
+start_url = "https://www.zeitoons.com/"
+
 # Signal handler for graceful exit on Ctrl+C
 def signal_handler(sig, frame):
     print("\n\nCtrl+C detected. Stopping the crawl process...")
-    logging.info(f"Total documents scraped: {len(articles_content)}")
-    save_json(articles_content, start_url=start_url)
+    logging.info(f"Total documents scraped: {len(articles)}")
+    save_json(articles, start_url=start_url)
     exit(0)
 
 # Register the signal handler for Ctrl+C (SIGINT)
 signal.signal(signal.SIGINT, signal_handler)
 
 def main():
+    global articles
     # setup logging
     setup_logging()
 
@@ -27,11 +32,9 @@ def main():
     # Get current timestamp
     timestamp = get_timestamp()
 
-        global crawl_count
+    crawl_count = 0
     # Starting URL and depth for the crawler
     
-    max_depth = 3  # Adjust the depth as needed
-
     parsed_start_url = urlparse(start_url)
     base_url = f"{parsed_start_url.scheme}://{parsed_start_url.netloc}"
 
@@ -43,8 +46,8 @@ def main():
     if not sitemap_urls:
         # If no sitemap URLs found, you might decide to proceed with recursive crawling
         logging.info(f"No sitemap URLs found for {base_url}. Proceeding with recursive crawling.")
-        crawl_website(start_url, 0, max_depth, headers, delay, rp)
-        return
+        articles = crawl_website(start_url, visited, rp)
+        return finalProcessing(articles)
 
     # Fetch and parse sitemaps to get URLs to crawl
     urls_to_crawl = fetch_and_parse_sitemaps(sitemap_urls)
@@ -52,32 +55,25 @@ def main():
     # Crawl the URLs
     for url in urls_to_crawl:
         # Check if URL is allowed by robots.txt
-        if rp is None or rp.can_fetch(DEFAULT_USER_AGENT, url) and (crawl_count <= MAX_CRAWL_COUNT or MAX_CRAWL_COUNT == -1):
+        if rp is None or rp.can_fetch(DEFAULT_USER_AGENT, url) and url not in visited and (crawl_count <= MAX_CRAWL_COUNT or MAX_CRAWL_COUNT == -1):
             doc = scrape_url(url)
+            visited.add(url)
             crawl_count += 1
-            articles_content.append(doc)
+            articles.append(doc)
 
-    logging.info(f"Total documents scraped: {len(articles_content)}")
+    logging.info(f"Total documents scraped: {len(articles)}")
 
-    """ data_to_save = [
-        {
-            "content": doc.content,
-            "url": doc.url
-        }
-        for doc in articles_content
-    ] """
+    return finalProcessing(articles)
 
+def finalProcessing(articles):
     
-
-    save_json(articles_content, start_url)
-
-    
-
-    if not articles_content:
+    if not articles:
         logging.error("No articles were scraped. Exiting.")
         return
+    
+    save_json(articles, start_url)
 
-
+    
 
 if __name__ == "__main__":
     main()
